a0b253ed3d4709d00aaef91369e3483871055cb9
Abdelrhman Kamal <abdelrhmankamaleliwa@gmail.com>
2022-06-20 16:38:37 +0200

    Initial Commit

---
+++ APIServiceComponent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIServiceComponent
+//
+//  Created by Abdelrhman Eliwa on 20/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
